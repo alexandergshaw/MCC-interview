db3ea194dbcc56bbe4e0d1572ca8dc37e784890a
Alex Shaw <alexandergshaw@gmail.com>
2023-01-19 15:21:04 -0600

    Add in database code

--- arrays_and_classes.py
+++ arrays_and_classes.py
@@ -35,38 +35,70 @@ misc = [0, '', False]
 
 # 1. Defining a class
 class Person:
-    def __init__(self, hair_color, top, baggage):
+    def __init__(self, hair_color, top, baggage, passenger_id):
         self.hair_color = hair_color
         self.top = top
         self.baggage = baggage
+        self.passenger_id = passenger_id
 
-    # def __str__(self):
-    #     return f"{self.hair_color}, {self.top}, {self.baggage}"
-    #
-    # def __repr__(self):
-    #     return 'Person(%s, %s, %b)' % (self.hair_color, self.top, self.baggage)
+    def __str__(self):
+        return f"{self.passenger_id} {self.hair_color}, {self.top}, {self.baggage}"
+
+    def __repr__(self):
+        return 'Person(%i, %s, %s, %i)' % (self.passenger_id, self.hair_color, self.top, self.baggage)
 
 
 # 2. Instantiating an object
-person_1 = Person("red", "t-shirt", True)
+# person_1 = Person("red", "t-shirt", True, 12345)
 #
 # print("Name of first person: ", person_1.name)
 #
 # print("Whole first person: ", person_1)
 
 # 3. Storing objects in list
-# person_2 = Person("Will", 28, 98765)
-# person_3 = Person("Wren", 41, 24680)
+# person_2 = Person("blonde", "blazer", True, 67890)
+# person_3 = Person("brunette", "overcoat", False, 24680)
 #
 # obj_arr = [person_2, person_3]
 #
 # print("people object array", obj_arr)
 
 
-# RELATIONAL DATABASE (SQL
+# RELATIONAL DATABASE (SQLite)
 
+# 1. Creating the database and table
+import sqlite3
+connection = sqlite3.connect('passengers.db')
+cursor = connection.cursor()
 
+# table = """CREATE TABLE PASSENGERS (
+#     PASSENGER_ID INTEGER not null primary key,
+#     HAIR_COLOR VARCHAR(20) not null,
+#     TOP VARCHAR(20) not null,
+#     BAGGAGE INTEGER not null )"""
+# cursor.execute(table)
+#
+# # 2. Inserting rows into table
+# cursor.execute("INSERT INTO PASSENGERS VALUES (123, 'red', 'peacoat', 0)")
+# cursor.execute("INSERT INTO PASSENGERS VALUES (456, 'brown', 'sportcoat', 1)")
+# cursor.execute("INSERT INTO PASSENGERS VALUES (789, 'blonde', 'hoodie', 0)")
+# connection.commit()
+# connection.close()
 
+# 3. Retrieving the data
+# cursor.execute("SELECT * FROM PASSENGERS")
+# passengerRecords = cursor.fetchall()
+# print("passengerRecords", passengerRecords)
+# print("passengerRecords[0]", passengerRecords[0])
+
+# 4. Storing the data in objects and a list
+# passengerList = []
+# 
+# for p in passengerRecords:
+#     passenger = Person(p[1], p[2], p[3], p[0])
+#     passengerList.append(passenger)
+# 
+# print("passengerList", passengerList)
 
 
 
